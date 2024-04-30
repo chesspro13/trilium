1152a8e214793920e51f7b09db9a6cb8eb32ee58
Brandon <brandon@mauldin314.com>
2024-04-29 18:54:40 -0700

    Updated build data

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -5,8 +5,8 @@ SERIES=${VERSION:0:4}-latest
 
 cat package.json | grep -v electron > server-package.json
 
-sudo docker build -t zadam/trilium:$VERSION --network host -t zadam/trilium:$SERIES .
+sudo docker build -t chesspro13/trilium:$VERSION --network host -t chesspro13/trilium:$SERIES .
 
 if [[ $VERSION != *"beta"* ]]; then
-  sudo docker tag zadam/trilium:$VERSION zadam/trilium:latest
+  sudo docker tag chesspro13/trilium:$VERSION chesspro13/trilium:latest
 fi
